65f840f5667514ba5f193f8391c88fffae72e02f
Dezang <dezang@dezang.net>
2020-03-01 04:16:38 +0900

    최초 커밋

---
+++ src/test/java/net/dezang/gsobject/GsObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dezang.gsobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
